6fd5cf1d2a0b910f2f35e0818dce58e82cf93d74
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-23 18:20:58 -0700

    Listed steps

--- romanNum.js
+++ romanNum.js
@@ -1,1 +1,6 @@
-console.log("script connected");
+// Define array of 7 divisors [1, 5, 10, 50, 100, 500, 1000]
+// Check magnitude of number to determine starting divisor (starting with largest divisor)
+// Count quantity of largest divisor
+// Check if number has any remainders
+// Iterate recursively to smaller orders of magnitude
+// Add subtractive notation
