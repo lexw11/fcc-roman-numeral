02fb1cba9f3a30c590f11a21372753aa903a5332
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-23 19:12:00 -0700

    Determined largest Roman numeral order of magnitude necessary to convert number

--- romanNum.js
+++ romanNum.js
@@ -4,3 +4,22 @@
 // Check if number has any remainders
 // Iterate recursively to smaller orders of magnitude
 // Add subtractive notation
+
+function convertToRoman(num) {
+	// Define order of magnitude arrays
+	const decs = [ 1, 5, 10, 50, 100, 500, 1000];
+	const roms = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+
+	// Sort num into decs array
+	let arr = decs.concat([num]).sort( (a, b) => a - b );
+	// Determine num's order of magnitude
+	let div = arr[ arr.indexOf(num) - 1 ];
+	
+	// Determine repetitions of largest order of magnitude
+	let rep = Math.floor( num / div );
+	// Create string with repeating Roman numerals
+	let str = roms[ decs.indexOf(div) ].repeat(rep);
+	console.log(str);
+}
+
+convertToRoman(36);
