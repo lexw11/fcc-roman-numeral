5b79e74d972da27128c4ef9803bf8fe646845678
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-24 01:33:53 -0700

    Fix handling of condition where num is evenly divisible by divisor

--- romanNum.js
+++ romanNum.js
@@ -15,9 +15,11 @@ function convertToRoman(num) {
 	function addRoman(num, decs, roms) {
 		// Sort num into decs array
 		let arr = decs.concat([num]).sort( (a, b) => a - b );
+		
 		// Determine num's order of magnitude
 		let div;
-		if( num === 1 ) {
+		// Take care of case where num exactly equal to one of the orders of magnitude
+		if( num === arr[ arr.indexOf(num) + 1 ] ) {
 			div = num;
 		} else {
 			div = arr[ arr.indexOf(num) - 1 ];
@@ -25,24 +27,27 @@ function convertToRoman(num) {
 		
 		// Determine repetitions of largest order of magnitude
 		let rep = Math.floor( num / div );
+		
 		// Create string with repeating Roman numerals
 		let str = roms[ decs.indexOf(div) ].repeat(rep);
 
-		if( div !== 1 ) {
+		// If num is not evenly divisible by divisor, iterate recursively over remainder
+		if( num % div !== 0 ) {
 			str += addRoman((num - div * rep), decs, roms);
 		}
+
 		return str;
 	}
 }
 
-// console.log(convertToRoman(2));
-// console.log(convertToRoman(3));
-// console.log(convertToRoman(4));
-// console.log(convertToRoman(5));
-// console.log(convertToRoman(9));
-// console.log(convertToRoman(12));
-// console.log(convertToRoman(16));
-// console.log(convertToRoman(29));
-// console.log(convertToRoman(44));
-// console.log(convertToRoman(68));
-// console.log(convertToRoman(400));
+console.log(convertToRoman(2));
+console.log(convertToRoman(3));
+console.log(convertToRoman(4));
+console.log(convertToRoman(5));
+console.log(convertToRoman(9));
+console.log(convertToRoman(12));
+console.log(convertToRoman(16));
+console.log("29 is " + convertToRoman(29));
+console.log("44 is " + convertToRoman(44));
+console.log("68 is " + convertToRoman(68));
+console.log("400 is " + convertToRoman(400));
