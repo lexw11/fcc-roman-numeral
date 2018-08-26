96e047c1a23d89cf1be96632baeaaa44780d3c02
lexw11 <36146160+lexw11@users.noreply.github.com>
2018-08-25 19:33:19 -0700

    New approach. Split number string into array and evaluate each digit individually. Passes all tests. Needs clean-up

--- romanNum.js
+++ romanNum.js
@@ -1,90 +1,42 @@
-// Define array of 7 divisors [1, 5, 10, 50, 100, 500, 1000]
-// Check magnitude of number to determine starting divisor (starting with largest divisor)
-// Count quantity of largest divisor
-// Check if number has any remainders
-// Iterate recursively to smaller orders of magnitude
-// Add subtractive notation
-
 function convertToRoman(num) {
-	// Define order of magnitude arrays
-	const decs = [ 1, 5, 10, 50, 100, 500, 1000];
-	const roms = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
-	// =================================================
-	// const roms = {
-	// 	1:    'I',
-	// 	5:    'V',
-	// 	10:   'X',
-	// 	50:   'L',
-	// 	100:  'C',
-	// 	500:  'D',
-	// 	1000: 'M'
-	// }
-	// =================================================
+	// Define Roman Numeral Characters
+	const roms = {
+		'0':    '',
+		'1':    'I',
+		'5':    'V',
+		'10':   'X',
+		'50':   'L',
+		'100':  'C',
+		'500':  'D',
+		'1000': 'M'
+	}
 
-	return addRoman(num, decs, roms);
-
-	function addRoman(num, decs, roms) {
-		let str;
-		// First check if num is exactly equal to one of the orders of magnitude
-		if( decs.includes(num) ) {
-			str = roms[ decs.indexOf(num) ];
-			return str;
+	// Split number into array of its digits
+	let arr = num.toString().split("").map( el => parseInt(el, 10) );
+	
+	// Convert each digit to corresponding Roman Numeral character
+	let arr2 = arr.map( function(dig, index, arr) {
+		// Determine order of magnitude of digit
+		let mag = arr.length - index - 1;
 		
+		// Calculate integer quotient and remainder of digit divided by 5
+		let numFive = Math.floor( dig / 5 );
+		let rmdr = dig % 5;
+
+		// Get lookup numbers for corresponding Roman Numeral characters
+		let powTen = Math.pow(10, mag);
+
+		let str;
+		// Handle case where remainder is 4
+		if( rmdr === 4 ) {
+			str = roms[ powTen.toString() ] + roms[ (powTen * (numFive + 1) * 5).toString() ];
 		} else {
-			// Sort num into decs array
-			let arr = decs.concat([num]).sort( (a, b) => a - b );
-			let numIndex = arr.indexOf(num);
-			
-			// Select greatest possible divisor from orders of magnitude array
-			let divIndex = numIndex - 1;
-			let div = arr[ divIndex ];
-
-			// Check to see if subtractive notation is necessary
-			let oneUp = arr[ numIndex + 1 ];
-			let gcd = getGcd(num, oneUp);
-			// if( (oneUp - num) / gcd === 1 ) {
-			// 	// Append Roman numeral for next order of magnitude to string
-			// 	str = roms[ decs.indexOf(gcd) ] + roms[ divIndex + 1 ];
-
-			// } else {
-				// Calculate quotient
-				let quot = Math.floor( num / div );
-				// Append repeating Roman numerals to string
-				str = roms[ divIndex ].repeat(quot); 
-
-				// Calculate remainder
-				let rmdr = num % div;
-				// If there's a remainder, apply function recursively to it
-				if( rmdr !== 0 ) {
-					str += addRoman(rmdr, decs, roms);
-				}
-			// }				
-
-			return str;
+			str = roms[ (powTen * numFive * 5).toString() ] + roms[ powTen.toString() ].repeat(rmdr);
 		}
-	}
-
-	// Find the greatest common divisor of two numbers
-	function getGcd(a, b) {
-		let num = Math.min(a, b);
-		for( let div = num; div > 0; div-- ) {
-			if( a % div === 0 && b % div === 0 ) {
-				return div;
-			}
-		}
-	}
+		return str;
+	});
+	
+	return arr2.join("");
 }
 
-// console.log(convertToRoman(2));
-// console.log(convertToRoman(3));
-// console.log(convertToRoman(4));
-// console.log(convertToRoman(5));
-// console.log(convertToRoman(9));
-// console.log(convertToRoman(12));
-// console.log(convertToRoman(16));
-// console.log("29 is " + convertToRoman(29));
-console.log("44 is " + convertToRoman(44));
-console.log("45 is " + convertToRoman(45));
-console.log("68 is " + convertToRoman(68));
-console.log("400 is " + convertToRoman(400));
-// console.log(convertToRoman(50));
+console.log(convertToRoman(44));
